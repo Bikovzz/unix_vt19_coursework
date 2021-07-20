712aee2d96cc5f264036ae1f0ed17c468ed4014a
Bikovzz <86464081+Bikovzz@users.noreply.github.com>
2021-07-21 00:50:46 +0300

    Update MainWindows.cs
    Еrrors corrected

--- MainWindows.cs
+++ MainWindows.cs
@@ -20,67 +20,63 @@ public partial class MainWindow : Gtk.Window
         a.RetVal = true;
     }
 
-    public String usd, eur, rub;
-
-    protected void OnBtnCalculateClicked(object sender, EventArgs e)
+    protected void OnButton2Clicked(object sender, EventArgs e)
     {
+        String usd, eur, rub;
         WebClient wc = new WebClient();
         String buff = wc.DownloadString("https://bank.gov.ua/ua/markets/exchangerates?date=19.07.2021&period=daily");
         usd = System.Text.RegularExpressions.Regex.Match(buff, @"""US Dollar"",""rate"":""([0-9]+\,[0-9]+)""").Groups[1].Value;
         eur = System.Text.RegularExpressions.Regex.Match(buff, @"""Euro"",""rate"":""([0-9]+\,[0-9]+)""").Groups[1].Value;
         rub = System.Text.RegularExpressions.Regex.Match(buff, @"""Russian Ruble"",""rate"":""([0-9]+\,[0-9]+)""").Groups[1].Value;
-        int k = 0;
+        double k = 0, dob_val;
+        string value;
         if (cmbFrom.ActiveText.ToString() == "EUR" && cmbTo.ActiveText.ToString() == "USD")
         {
-            k = Convert.ToInt32(eur) / Convert.ToInt32(usd);
+            k = Convert.ToDouble(eur) / Convert.ToDouble(usd);
         }
         else if (cmbFrom.ActiveText.ToString() == "EUR" && cmbTo.ActiveText.ToString() == "RUB")
         {
-            k = Convert.ToInt32(eur) / Convert.ToInt32(rub);
+            k = Convert.ToDouble(eur) / Convert.ToDouble(rub);
         }
         else if (cmbFrom.ActiveText.ToString() == "EUR" && cmbTo.ActiveText.ToString() == "UAH")
         {
-            k = Convert.ToInt32(eur);
+            k = Convert.ToDouble(eur);
         }
         else if (cmbFrom.ActiveText.ToString() == "USD" && cmbTo.ActiveText.ToString() == "EUR")
         {
-            k = Convert.ToInt32(usd) / Convert.ToInt32(eur);
+            k = Convert.ToDouble(usd) / Convert.ToDouble(eur);
         }
         else if (cmbFrom.ActiveText.ToString() == "USD" && cmbTo.ActiveText.ToString() == "RUB")
         {
-            k = Convert.ToInt32(usd) / Convert.ToInt32(rub);
+            k = Convert.ToDouble(usd) / Convert.ToDouble(rub);
         }
         else if (cmbFrom.ActiveText.ToString() == "USD" && cmbTo.ActiveText.ToString() == "UAH")
         {
-            k = Convert.ToInt32(usd);
+            k = Convert.ToDouble(usd);
         }
         else if (cmbFrom.ActiveText.ToString() == "RUB" && cmbTo.ActiveText.ToString() == "EUR")
         {
-            k = Convert.ToInt32(rub) / Convert.ToInt32(eur);
+            k = Convert.ToDouble(rub) / Convert.ToDouble(eur);
         }
         else if (cmbFrom.ActiveText.ToString() == "RUB" && cmbTo.ActiveText.ToString() == "USD")
         {
-            k = Convert.ToInt32(rub) / Convert.ToInt32(usd);
+            k = Convert.ToDouble(rub) / Convert.ToDouble(usd);
         }
         else if (cmbFrom.ActiveText.ToString() == "RUB" && cmbTo.ActiveText.ToString() == "UAH")
         {
-            k = Convert.ToInt32(rub);
+            k = Convert.ToDouble(rub);
         }
         else if (cmbFrom.ActiveText.ToString() == "UAH" && cmbTo.ActiveText.ToString() == "RUB")
         {
-            k = 1 / Convert.ToInt32(rub);
+            k = 1 / Convert.ToDouble(rub);
         }
         else if (cmbFrom.ActiveText.ToString() == "UAH" && cmbTo.ActiveText.ToString() == "USD")
         {
-            k = 1 / Convert.ToInt32(usd);
+            k = 1 / Convert.ToDouble(usd);
         }
         else if (cmbFrom.ActiveText.ToString() == "RUB" && cmbTo.ActiveText.ToString() == "EUR")
         {
-            k = 1 / Convert.ToInt32(usd);
-        }
-        else
-        {
-            k = 0;
+            k = 1 / Convert.ToDouble(usd);
         }
     }
 }
