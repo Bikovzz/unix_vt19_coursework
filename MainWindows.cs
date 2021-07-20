c5505bc31aaf8606e03fdb08626ee1f5f15a7d70
Bikovzz <86464081+Bikovzz@users.noreply.github.com>
2021-07-20 22:58:11 +0300

    Update MainWindows.cs
    Add regular expressions for parsing exchange rate

--- MainWindows.cs
+++ MainWindows.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.;
+using System.IO;
+using System.Net;
 using Gtk;
 
-public class 
+
 public partial class MainWindow : Gtk.Window
 {
     public MainWindow() : base(Gtk.WindowType.Toplevel)
@@ -15,8 +20,14 @@ public partial class MainWindow : Gtk.Window
         a.RetVal = true;
     }
 
+    public String d, e, r;
+
     protected void OnBtnCalculateClicked(object sender, EventArgs e)
     {
-
+        WebClient wc = new WebClient();
+        String buff = wc.DownloadString("https://bank.gov.ua/ua/markets/exchangerates?date=30.09.2020&period=daily");
+        d = System.Text.RegularExpressions.Regex.Match(buff, @"""US Dollar"",""rate"":""([0-9]+\,[0-9]+)""").Groups[1].Value;
+        e = System.Text.RegularExpressions.Regex.Match(buff, @"""Euro"",""rate"":""([0-9]+\,[0-9]+)""").Groups[1].Value;
+        r = System.Text.RegularExpressions.Regex.Match(buff, @"""Russian Ruble"",""rate"":""([0-9]+\,[0-9]+)""").Groups[1].Value;
     }
 }
